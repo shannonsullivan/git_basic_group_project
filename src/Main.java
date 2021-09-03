ffff3966694195fb4c21ea64dc728899b5dd4591
Byron Mackay <byronmackay@lambdaschool.com>
2021-09-02 22:50:44 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a simple addition problem.");
+        final int result = 2 - 1;
+        System.out.println("2 + 1 = " + result);
+    }
+    
+}
