d7e3bce5ce6b78f22873e73e72e83104dc0a0ecd
shannonsullivan <shannons4252@gmail.com>
2021-09-29 19:52:41 -0400

    Fixed math problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a simple addition problem.");
 
-        int result = 2 - 1;
+        int result = 2 + 1;
 
         System.out.println("2 + 1 = " + result);
     }
